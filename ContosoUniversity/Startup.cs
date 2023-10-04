ff2445c0dff14b2a9b595cc7779d03b842a14f2b
Patrick <Patrick@DESKTOP-7BVVI1C>
2023-10-04 19:23:23 +0300

    Register the "SchoolContext" in startup.cs

--- ContosoUniversity/Startup.cs
+++ ContosoUniversity/Startup.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Builder;
+using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -23,15 +27,17 @@ namespace ContosoUniversity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SchoolContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage(); // This middleware should be configured here
             }
             else
             {
